e7c11a503cc13b4014eba39cbfa6909dc72e4865
Piotrek <szczypiorofix@o2.pl>
2019-08-26 13:28:11 +0200

    Deploy update #10

--- deploy_server.js
+++ deploy_server.js
@@ -11,7 +11,7 @@ var serverAppConfig = {
     remoteRoot: "/public_nodejs",
     deleteRemote: false,
     include: ['*'],
-    exclude: ['node_modules/**/*', 'tslint.json', 'tsconfig.json', 'app.ts']
+    exclude: ['/node_modules/**/', 'tslint.json', 'tsconfig.json', 'app.ts']
 }
 
 
